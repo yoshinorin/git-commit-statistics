1a5e0bed0d169e33e68cd558053b77ffa0a66da4
YoshinoriN <nayo-msoft@outlook.jp>
2018-06-28 01:29:01 +0900

    Restracture command and options module

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 
 const months = require('./lib/months');
 const weekday = require('./lib/weekday');
-const options = require('./lib/options');
-const commands = require('./lib/commands');
+const commands = require('./lib/command');
 
 class CommitStatistics {
 
@@ -18,33 +17,6 @@ class CommitStatistics {
     }
     this._path = path;
   }
-
-  /**
-   * Command builder for git log.
-   *
-   * @param {Object} opts git log options
-   */
-  commandBuilder(opts) {
-    let command = "";
-    command = commands.basic;
-
-    if (opts === undefined) {
-      return command;
-    }
-
-    Object.entries(opts).forEach(opt => {
-      let x = options.list.find(function(v) {
-        return v === opt[0];
-      });
-      if (x === undefined) {
-        throw new Error(`Option ${opt[0]} is invalid.`);
-      }
-      command += ` --${opt[0]}="${opt[1]}"`;
-    });
-
-    return command;
-  }
-
 }
 
 module.exports = CommitStatistics;
