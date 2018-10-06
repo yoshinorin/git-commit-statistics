41d90faede9895f0ed313aef9455ec52c99f9560
YoshinoriN <nayo-msoft@outlook.jp>
2018-10-06 23:23:46 +0900

    Delete old test

--- tests/commitStatistics.test.js
+++
@@ -1,6 +0,0 @@
-const CommitStatistics = require('../index');
-
-test('commandBuilder', () => {
-  let cs = new CommitStatistics("/test/example/.git");
-  expect(cs.commandBuilder()).toBe("git --git-dir /test/example/.git");
-});
