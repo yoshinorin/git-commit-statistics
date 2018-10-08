65048279f6ff40ac05dd0b1e0ebab7760d8af687
YoshinoriN <nayo-msoft@outlook.jp>
2018-10-08 23:01:20 +0900

    Implement groupBy

--- src/index.js
+++ src/index.js
@@ -48,6 +48,21 @@ class CommitStatistics {
     }
   }
 
+  groupBy(list, field) {
+    return list.reduce((result, current) => {
+      const y = result.find((x) => x[field] === current[field]);
+      if (y) {
+        y.count ++;
+      } else {
+        result.push({
+          [field]: current[field],
+          count: 1
+        });
+      }
+      return result;
+    }, []);
+  }
+
 }
 
 module.exports = CommitStatistics;
